db820dee3ea5b6aea8457b6b2f55e27272321c4b
0s1n <jpablo_166@hotmail.com>
2024-07-30 20:14:49 -0500

    refactor: deleted NxWelcome Component & added MainNavComponent

--- apps/angular-ecommerce/src/app/app.component.ts
+++ apps/angular-ecommerce/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MainNavComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
