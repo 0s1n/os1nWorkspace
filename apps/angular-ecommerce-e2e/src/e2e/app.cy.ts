0da0382ccbc58409d09e5af738586352d492085d
0s1n <jpablo_166@hotmail.com>
2024-07-30 20:19:01 -0500

    feat(test): added Cypress & tested Navbar

--- apps/angular-ecommerce-e2e/src/e2e/app.cy.ts
+++ apps/angular-ecommerce-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
-
 describe('angular-ecommerce-e2e', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  it('should display navigation', () => {
+    cy.visit('http://localhost:4200');
+    cy.contains('span', 'angular-ecommerce');
   });
 });
