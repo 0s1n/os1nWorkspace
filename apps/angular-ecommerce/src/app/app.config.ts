f345a0e0de0a1a6d2bb8ec962f4870f10d7438bd
0s1n <jpablo_166@hotmail.com>
2024-08-06 00:46:36 -0500

    feat: added category state on angular-ecommerce

--- apps/angular-ecommerce/src/app/app.config.ts
+++ apps/angular-ecommerce/src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import {
+  CATEGORY_FEATURE_KEY,
+  CategoryEffects,
+  categoryReducer,
+} from '@os1n-workspace/category';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+// import * as fromCategory2 from './+state/category2.reducer';
+// import { Category2Effects } from './+state/category2.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(CategoryEffects),
+    provideState(CATEGORY_FEATURE_KEY, categoryReducer),
+    provideHttpClient(),
+    provideStore(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(),
-  ],
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
